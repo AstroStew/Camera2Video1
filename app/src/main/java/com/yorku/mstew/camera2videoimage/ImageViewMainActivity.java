dfe1ec3a6cbe9750b8b3508dbcf13926052def7b
Michael Stewart <mstewart@yorku.ca>
2017-06-22 18:29:01 -0400

    end of thursday commit

---
+++ app/src/main/java/com/yorku/mstew/camera2videoimage/ImageViewMainActivity.java
@@ -0,0 +1,19 @@
+package com.yorku.mstew.camera2videoimage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mstew on 2017-06-22.
+ */
+
+public class ImageViewMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.)
+    }
+
+
+
+}
