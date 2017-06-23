cf7754fedb9840873364dea2b3eefb2348ab5e35
Michael Stewart <mstewart@yorku.ca>
2017-06-23 13:32:52 -0400

    improved image Viewer

--- app/src/main/java/com/yorku/mstew/camera2videoimage/Page.java
+++ app/src/main/java/com/yorku/mstew/camera2videoimage/Page.java
@@ -1,11 +1,18 @@
 package com.yorku.mstew.camera2videoimage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class Page extends AppCompatActivity {
     ImageView mImageView;
@@ -17,14 +24,14 @@ public class Page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view_main);
-        closepage=(ImageButton)findViewById(R.id.pageclose);
+        /*closepage=(ImageButton)findViewById(R.id.pageclose);
         closepage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent closepage=new Intent(getApplicationContext(), Camera2VideoImageActivity.class);
                 startActivity(closepage);
             }
-        });
+        });*/
         mImageView=(ImageView)findViewById(R.id.ImageView);
         Intent intent=new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
@@ -33,7 +40,30 @@ public class Page extends AppCompatActivity {
 
 
 
+    }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent resultData){
+        if(requestCode==REQUEST_OPEN_RESULT_CODE && resultCode == RESULT_OK){
+            Uri uri=null;
+            if(resultData != null){
+                uri=resultData.getData();
 
+                try {
+                    Bitmap bitmap = getBitmapFromUri(uri);
+                    mImageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+    private Bitmap getBitmapFromUri(Uri uri)throws IOException{
+        ParcelFileDescriptor parcelFileDescriptor=getContentResolver().openFileDescriptor(uri,"r");
+        FileDescriptor fileDescriptor=parcelFileDescriptor.getFileDescriptor();
+        Bitmap bitmap= BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return bitmap;
 
     }
 }
