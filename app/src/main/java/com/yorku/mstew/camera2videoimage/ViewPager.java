ba91f7fdd5b7de8c837800f2296ded8789825c58
Michael Stewart <mstewart@yorku.ca>
2017-06-25 18:00:27 -0400

    ViewPager almost complete-layout files still need work

--- app/src/main/java/com/yorku/mstew/camera2videoimage/ViewPager.java
+++
@@ -1,8 +0,0 @@
-package com.yorku.mstew.camera2videoimage;
-
-/**
- * Created by mstew on 2017-06-24.
- */
-
-public class ViewPager {
-}
---
+++ app/src/main/java/com/yorku/mstew/camera2videoimage/ViewPagerMainActivity.java
@@ -0,0 +1,184 @@
+package com.yorku.mstew.camera2videoimage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by mstew on 2017-06-25.
+ */
+
+public class ViewPagerMainActivity extends AppCompatActivity {
+        private ViewPager viewPager;
+    Button next,skip;
+    private ViewPagerManager viewPagerManager;
+    private int[] layouts;
+    private TextView[] dots;
+    private LinearLayout dotsLayout;
+    private ViewPagerAdapter viewpageradapter;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewPagerManager=new ViewPagerManager(this);
+        if(!viewPagerManager.Check()){
+            viewPagerManager.setFirst(false);
+            Intent i=new Intent(ViewPagerMainActivity.this,ViewPagerMainActivity2.class);
+            startActivity(i);
+            finish();
+        }
+        if(Build.VERSION.SDK_INT>=21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            );
+        }
+
+        setContentView(R.layout.fragment0);
+
+        viewPager=(ViewPager)findViewById(R.id.view_pager);
+        dotsLayout=(LinearLayout)findViewById(R.id.layoutDots);
+        skip=(Button) findViewById(R.id.btn_skip);
+        next=(Button)findViewById(R.id.btn_next);
+
+
+
+        layouts=new int[]{R.layout.fragment1,R.layout.fragment2,R.layout.fragment3};
+        addButtonDots(0);
+        changeStatusBarBolor();
+        viewpageradapter=new ViewPagerAdapter();
+        viewPager.setAdapter(viewpageradapter);
+        viewPager.addOnPageChangeListener(viewListener);
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(ViewPagerMainActivity.this,ViewPagerMainActivity2.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current=getItem(+1);
+                if(current<layouts.length){
+                   viewPager.setCurrentItem(current);
+                }else{
+                    Intent i =new Intent(ViewPagerMainActivity.this,ViewPagerMainActivity2.class);
+                    startActivity(i);
+                    finish();
+                }
+
+            }
+        });
+
+    }
+
+    private void addButtonDots(int position){
+        dots=new TextView[layouts.length];
+        int[] colorActive=getResources().getIntArray(R.array.dot_active);
+        int[] colorInactive=getResources().getIntArray(R.array.got_innactive);
+        dotsLayout.removeAllViews();
+        for(int i=0; i<dots.length;i++){
+            dots[i]=new TextView(this);
+            dots[i].setText(Html.fromHtml("&/8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorInactive[position]);
+            dotsLayout.addView(dots[i]);
+
+        }
+        if(dots.length>0){
+            dots[position].setTextColor(colorActive[position]);
+        }
+
+
+
+    }
+    private int getItem(int i){
+        return viewPager.getCurrentItem()+1;
+    }
+
+
+    ViewPager.OnPageChangeListener viewListener= new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addButtonDots(position);
+            if(position==layouts.length-1){
+                next.setText("Proceed");
+                skip.setVisibility(View.GONE);
+
+            }else{
+                next.setText("NEXT");
+                skip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+    private void changeStatusBarBolor(){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            Window window=getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+ public class ViewPagerAdapter extends PagerAdapter{
+
+     private LayoutInflater layoutInflater;
+
+
+     @Override
+     public int getCount() {
+         return layouts.length;
+     }
+
+     @Override
+     public Object instantiateItem(ViewGroup container, int position) {
+         layoutInflater=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+         View v= layoutInflater.inflate(layouts[position],container,false);
+         container.addView(v);
+
+         return v;
+     }
+
+     @Override
+     public void destroyItem(ViewGroup container, int position, Object object) {
+         View v=(View)object;
+         container.removeView(v);
+     }
+
+     @Override
+     public boolean isViewFromObject(View view, Object object) {
+         return view==object;
+     }
+
+ }
+
+
+}
