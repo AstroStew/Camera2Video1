00b5fc60bd0416145cbd2e787cd31c2f9e62e544
Michael Stewart <mstewart@yorku.ca>
2017-07-27 16:44:05 -0400

    minor Intro Page chnages

--- app/src/main/java/com/yorku/mstew/camera2videoimage/TabedMenuActivity.java
+++ app/src/main/java/com/yorku/mstew/camera2videoimage/TabedMenuActivity.java
@@ -86,6 +86,10 @@ public class TabedMenuActivity extends AppCompatActivity {
             Intent ImageViewIntent=new Intent(getApplicationContext(), Page.class);
             startActivity(ImageViewIntent);
         }
+        if(id==R.id.Tutbutton){
+            Intent i=new Intent(getApplicationContext(),ViewPagerMainActivity.class);
+            startActivity(i);
+        }
 
         return super.onOptionsItemSelected(item);
     }
