372467a2b04504eab0b358dd15fa4819ff3c71bf
Michael Stewart <mstewart@yorku.ca>
2017-07-28 22:21:38 -0400

    Trying to chnage font-doesn't work

--- app/build/generated/source/r/debug/android/support/design/R.java
+++ app/build/generated/source/r/debug/android/support/design/R.java
@@ -665,19 +665,19 @@ public final class R {
         public static final int design_fab_background = 0x7f02005b;
         public static final int design_ic_visibility = 0x7f02005c;
         public static final int design_snackbar_background = 0x7f02005d;
-        public static final int navigation_empty_icon = 0x7f020085;
-        public static final int notification_action_background = 0x7f020086;
-        public static final int notification_bg = 0x7f020087;
-        public static final int notification_bg_low = 0x7f020088;
-        public static final int notification_bg_low_normal = 0x7f020089;
-        public static final int notification_bg_low_pressed = 0x7f02008a;
-        public static final int notification_bg_normal = 0x7f02008b;
-        public static final int notification_bg_normal_pressed = 0x7f02008c;
-        public static final int notification_icon_background = 0x7f02008d;
-        public static final int notification_template_icon_bg = 0x7f020092;
-        public static final int notification_template_icon_low_bg = 0x7f020093;
-        public static final int notification_tile_bg = 0x7f02008e;
-        public static final int notify_panel_notification_icon_bg = 0x7f02008f;
+        public static final int navigation_empty_icon = 0x7f020086;
+        public static final int notification_action_background = 0x7f020087;
+        public static final int notification_bg = 0x7f020088;
+        public static final int notification_bg_low = 0x7f020089;
+        public static final int notification_bg_low_normal = 0x7f02008a;
+        public static final int notification_bg_low_pressed = 0x7f02008b;
+        public static final int notification_bg_normal = 0x7f02008c;
+        public static final int notification_bg_normal_pressed = 0x7f02008d;
+        public static final int notification_icon_background = 0x7f02008e;
+        public static final int notification_template_icon_bg = 0x7f020093;
+        public static final int notification_template_icon_low_bg = 0x7f020094;
+        public static final int notification_tile_bg = 0x7f02008f;
+        public static final int notify_panel_notification_icon_bg = 0x7f020090;
     }
     public static final class id {
         public static final int action0 = 0x7f0e00ce;
